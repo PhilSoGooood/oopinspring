f92dfc82bb6a8cee65020b1ba8122dfa9f8838da
PhilSoGooood <peelhw@gmail.com>
2021-12-27 17:09:29 +0900

    instanceof 연산자

---
+++ src/ch4/instanceOf01/Driver.java
@@ -0,0 +1,27 @@
+package ch4.instanceOf01;
+
+class 동물 {}
+class 조류 extends 동물 {}
+class 펭귄 extends 조류 {}
+
+public class Driver {
+	public static void main(String[] args) {
+		동물 동물객체 = new 동물();
+		조류 조류객체 = new 조류();
+		펭귄 펭귄객체 = new 펭귄();
+
+
+		//instanceOf : 객체가 특정 클래스의 인스턴스인지 물어보는 연산자. 결과로 true 또는 false 를 반환
+		System.out.println(동물객체 instanceof 동물);
+
+		System.out.println(조류객체 instanceof 동물);
+		System.out.println(조류객체 instanceof 조류);
+
+		System.out.println(펭귄객체 instanceof 동물);
+		System.out.println(펭귄객체 instanceof 펭귄);
+		System.out.println(펭귄객체 instanceof 조류);
+
+		System.out.println(펭귄객체 instanceof Object);
+
+	}
+}
