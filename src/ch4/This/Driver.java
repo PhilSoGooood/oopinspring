33835468582b1baacd89b89684cde3899cb4e473
PhilSoGooood <peelhw@gmail.com>
2021-12-28 13:30:54 +0900

    this 키워드

---
+++ src/ch4/This/Driver.java
@@ -0,0 +1,18 @@
+package ch4.This;
+
+class 펭귄 {
+	int var = 10;
+
+	void test() {
+		int var = 20;
+
+		System.out.println(var);
+		System.out.println(this.var);
+	}
+}
+
+public class Driver{
+	public static void main(String[] args) {
+
+	}
+}
